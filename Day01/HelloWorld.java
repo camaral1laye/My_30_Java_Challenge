89af9e3f58977b4700dbfe95f0a3934d0c175241
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-10-19 15:37:25 +0000

    Day01

---
+++ Day01/HelloWorld.java
@@ -0,0 +1,10 @@
+package Day01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+        //output
+        System.out.println("Hello, World!");
+        System.out.println("Here my first Java program");
+    }
+}
