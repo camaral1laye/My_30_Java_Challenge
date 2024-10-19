b9209ed4e56c2e66a532ccce12417e93bd725a4e
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-10-19 18:18:07 +0000

    updated Laye.java

--- Day02/Laye.java
+++ Day02/Laye.java
@@ -3,9 +3,9 @@ package Day02;
 public class Laye {
     public static void main(String[] args) {
         
-        //System.out.println("Hello From Laye!");
+        //output
         sayHi();
-       
+    
     }
     public static void sayHi() {
         System.out.println("sayHi: Congratualations!");
