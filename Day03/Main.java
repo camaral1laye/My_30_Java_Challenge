94686e5238886de018a41943771debfec2452dfc
Laye Camara <150172347+camaral1laye@users.noreply.github.com>
2024-10-20 16:45:29 +0000

    update Day3

---
+++ Day03/Main.java
@@ -0,0 +1,9 @@
+package Day03;
+
+public class Main {
+    public static void main(String[] args){
+        Me.sayName();
+        Me.sayAge();
+    }
+    
+}
---
+++ Day03/Me.java
@@ -0,0 +1,16 @@
+package Day03;
+
+public class Me {
+    public static void main(String[] args) {
+      
+        sayName();
+        sayAge();
+    }
+    public static void sayName(){
+        System.out.println("My name: Laye Camara");
+    }
+    public static void sayAge(){
+        System.out.println("My age: 25");
+    }
+    
+}
